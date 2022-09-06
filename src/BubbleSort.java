bc847f8e6dd180a13219fbca259f78b2ebe2b5d5
Vashu Agarwal <vashuag9@gmail.com>
2022-09-06 14:17:01 +0530

    Bubbble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,9 +1,59 @@
+import java.util.Scanner;
 
-public class BubbleSort {
+//
+//public class BubbleSort {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
-
-}
+public class BubbleSort {  
+    static void bubbleSort(int[] arr) {  
+        int n = arr.length;  
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+         }  
+  
+    }  
+    public static void main(String[] args) {  
+    	Scanner sc = new Scanner(System.in);
+    	int n ;
+    	n = sc.nextInt();
+    	
+    	int[] arr = new int[n];
+    
+    	for(int i = 0;i<n;i++) {
+    	
+    		 arr[i] = sc.nextInt();
+    	}
+    	
+    	
+                 
+                System.out.println("Array Before Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+                System.out.println();  
+                  
+                bubbleSort(arr);//sorting array elements using bubble sort  
+                 
+                System.out.println("Array After Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+   
+        }  
+}  
